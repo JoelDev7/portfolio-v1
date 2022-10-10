f62d3783cbaa6639df07fe68358606bb83e1e2af
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2022-10-10 18:06:40 -0500

    about section created, responsive behaviour implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 window.addEventListener('load', ()=> {
     toggleNavVisibility();
+    setAboutHeight();
 });
 
 function toggleNavVisibility() {
-    const navbar = document.getElementById('navbar');
     const navList = document.getElementById('navList');
     const hambMenu = document.getElementById('hambMenu');
     hambMenu.addEventListener('click', () => {
         navList.classList.toggle('hidden');
     });
+}
+
+function setAboutHeight() {
+    const about = document.getElementById('about');
+    const windowHeight = isNaN(window.innerHeight) ? window.clientHeight : window.innerHeight;
+    const pageHeaderHeight = document.getElementById('pageHeader').clientHeight;
+    about.style.height = (windowHeight - pageHeaderHeight)+'px'; //never forget 'px'
+    about.style.marginTop = pageHeaderHeight + 'px';
 }
