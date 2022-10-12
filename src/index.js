3bd0ccf4e063af85d255edd4892d3d3ca551a24d
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2022-10-11 20:21:56 -0500

    Experience section created and responsive vehaviour implemented

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 window.addEventListener('load', ()=> {
-    toggleNavVisibility();
-    setAboutHeight();
+    toggleNavMenuVisibility();
+    setSectionHeight('about');
+    setSectionHeight('experience');
+    // normalizeTextWidth('fullname', ['im']);
 });
 
-function toggleNavVisibility() {
+/**
+ * Shows an hides navigation links when browsing on small screens.
+ */
+function toggleNavMenuVisibility() {
     const navList = document.getElementById('navList');
     const hambMenu = document.getElementById('hambMenu');
     hambMenu.addEventListener('click', () => {
         navList.classList.toggle('hidden');
     });
 }
-
-function setAboutHeight() {
-    const about = document.getElementById('about');
+/**
+ * Dynamically sets section height according to viewport height and header height.
+ * @param {*} sectionName 
+ */
+function setSectionHeight(sectionName) {
+    const section = document.getElementById(sectionName);
     const windowHeight = isNaN(window.innerHeight) ? window.clientHeight : window.innerHeight;
     const pageHeaderHeight = document.getElementById('pageHeader').clientHeight;
-    about.style.height = (windowHeight - pageHeaderHeight)+'px'; //never forget 'px'
-    about.style.marginTop = pageHeaderHeight + 'px';
+    section.style.height = (windowHeight - pageHeaderHeight)+'px'; //never forget 'px'
+    section.style.marginTop = pageHeaderHeight + 'px';
 }
+
+// /**
+//  * Receives a text element id, gets its width and sets that width to other text elements.
+//  * @param {*} refereceTextId Id of the reference text element.
+//  * @param {Array} targetText Array of ids from target text elements.
+//  */
+// function normalizeTextWidth(refereceTextId, targetTextId) {
+//     targetTextId.forEach(element => {
+//         document.getElementById(element).style.width = document.getElementById(refereceTextId).clientWidth + 'px';
+//     });
+// }
